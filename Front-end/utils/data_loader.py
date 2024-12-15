5a8556361a7d763f058c8a950510493f2a71ce75
soulcarus <joaoicaromoreira@gmail.com>
2024-12-15 00:33:27 -0300

    removing old structure files

--- Front-end/utils/data_loader.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_data(file_path):
-    """Load dataset from a given file path."""
-    try:
-        data = pd.read_csv(file_path)
-        return data
-    except FileNotFoundError:
-        raise Exception(f"File not found at {file_path}")
