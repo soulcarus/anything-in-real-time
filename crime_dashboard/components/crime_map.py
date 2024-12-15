e93f8228d2faf9eeb4b9b6bb1e25bf5374af9f9b
soulcarus <joaoicaromoreira@gmail.com>
2024-12-15 13:39:12 -0300

    zoom crime map

--- crime_dashboard/components/crime_map.py
+++ crime_dashboard/components/crime_map.py
@@ -2,7 +2,7 @@ import streamlit as st
 import plotly.express as px
 
 def render_crime_map(filtered_data):
-    st.header("Taxas de Crime Violento por Estado (Último Ano)")
+    st.header("Violent Crime Rates by State (Last Year)")
     latest_year = filtered_data['year'].max()
     latest_data = filtered_data[filtered_data['year'] == latest_year]
     latest_data['violent_crime_rate'] = latest_data['violent_crime'] / latest_data['population'] * 100000
@@ -12,7 +12,13 @@ def render_crime_map(filtered_data):
                             locationmode="USA-states", 
                             color='violent_crime_rate',
                             scope="usa",
-                            title=f"Taxa de Crime Violento por 100.000 Habitantes ({latest_year.year})",
+                            title=f"Violent Crime Rate per 100,000 People ({latest_year.year})",
                             color_continuous_scale="Viridis")
-    st.plotly_chart(fig_map)
-
+    fig_map.update_layout(
+        autosize=True,
+        width=1000,
+        height=700,
+        margin={"r":0,"t":40,"l":0,"b":0}
+    )
+    
+    st.plotly_chart(fig_map, use_container_width=True)
